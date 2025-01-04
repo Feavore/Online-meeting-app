262b6c1974c88f7565b5da5d33828d0620da5f73
DoLamMinhKhoi <111213348+DoLamMinhKhoi@users.noreply.github.com>
2025-01-04 23:30:04 +0700

    Add files via upload

---
+++ SIU_meeting/src/main/java/siu/ltmmt/test/HomeGUI.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package siu.ltmmt.test;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+import java.net.*;
+
+/**
+ *
+ * @author Latitude 5500
+ */
+public class HomeGUI extends JFrame {
+    
+    public static void main(String[] args) {
+        // Define the path to the image
+  
+    }
+}
